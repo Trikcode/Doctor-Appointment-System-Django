f0e6fcf571cd87793a3649c7e023c9c0f40705b8
Software Developer <ayesiganobert@gmail.com>
2022-09-22 07:55:36 +0300

    Update settings.py

--- MyProject/settings.py
+++ MyProject/settings.py
@@ -18,8 +18,7 @@ TEMPLATE_DIR = os.path.join(BASE_DIR, "templates")
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'jk+i9p!-!o4b*m@0nba-x7sl^b%_(6^oio84(xn&(l#5-68ay@'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
